0b7472bd081a02900e642428e810c506517e4081
matloff <matloff@cs.ucdavis.edu>
2016-05-04 10:41:43 -0700

    manipulations with gvar, var1 wrong, fixed now

--- tourn/R/gender.R
+++ tourn/R/gender.R
@@ -5,14 +5,20 @@
 
 gender <- function(df, gvar, var1)
 {
-	var1.name <- deparse(substitute(var1))
-	gvar <- eval(substitute(gvar),df,parent.frame())
+        ## changes by NM
+	## var1.name <- deparse(substitute(var1))
+	## gvar <- eval(substitute(gvar),df,parent.frame())
+        var1.name <- var1
 
-	gmale <- df[gvar == "male",]
-	gfemale <- df[gvar == "female",]
+        ## changes by NM
+	gmale <- df[df[[gvar]] == "male",]
+	gfemale <- df[df[[gvar]] == "female",]
 
-	varmale <- eval(substitute(var1),gmale,parent.frame())
-	varfemale <- eval(substitute(var1),gfemale,parent.frame())
+        ## changes by NM
+	## varmale <- eval(substitute(var1),gmale,parent.frame())
+	## varfemale <- eval(substitute(var1),gfemale,parent.frame())
+        varmale <- gmale[[var1]]
+        varfemale <- gfemale[[var1]]
 
 	dev.new()
 	aplot <- density(varmale)
