6bdf66a8a6ee2625834bd52179722693fb3f604c
Ariel Shin <Ariel@Ariel-Shins-MacBook-Pro-2.local>
2016-06-01 17:53:44 -0700

    User can choose from files -- correct version

--- tourn/Shiny/app4/ui.R
+++ tourn/Shiny/app4/ui.R
@@ -1,16 +1,22 @@
 library(shiny)
+gold <- read.csv("data/gold.csv")
+
 
 shinyUI(fluidPage(
   sidebarPanel(
         selectInput("dataset", "Choose a file:",
-            choices = c("Grapevine", "Gold", "Gold Plus", "Platinum")),
+            choices = c("Grapevine", "Gold", "Gold Plus", "Platinum"),
+            selected = "Gold",
+        ),
 
-        downloadButton('downloadData', 'Download')
+        selectInput("inSelect", "Choose a variable to plot: ", choices = names(gold), selected = names(gold)[1]),
+
+  		selectInput("inSelect2", "Choose a second variable (grouping variable) to plot: ", choices = names(gold), selected = names(gold)[2]),
+
+  		actionButton("createg", "Create graphs")
   ),
 
   mainPanel(
-        tableOutput('table')
+    plotOutput("plot1")
   )
-
 ))
-
